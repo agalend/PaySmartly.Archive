48980bf5940305f5ac57ffa62ad79ffc4b1afbf6
stefan <stefan.bozov@outlook.com>
2023-12-01 14:21:41 +0200

    better endpoint paths

--- PaySmartly.Archive/Endpoints/PaySlipEndpoints.cs
+++ PaySmartly.Archive/Endpoints/PaySlipEndpoints.cs
@@ -2,11 +2,11 @@ namespace PaySmartly.Archive.Endpoints
 {
     public static class PaySlipEndpoints
     {
-        public static PaySlipEndpoint GetEndpoint = new("/pay-slips/{id}", "get-pay-slip-by-id", "GET");
-        public static PaySlipEndpoint DeleteEndpoint = new("/pay-slips/{id}", "delete-pay-slip-by-id", "DELETE");
+        public static PaySlipEndpoint GetEndpoint = new("archive/pay-slips/{id}", "get-pay-slip-by-id", "GET");
+        public static PaySlipEndpoint DeleteEndpoint = new("archive/pay-slips/{id}", "delete-pay-slip-by-id", "DELETE");
 
-        public static PaySlipEndpoint GetAllPaySlipsForEmployeeEndpoint = new("/pay-slips/employee", "get-all-pay-slips-for-employee", "GET");
-        public static PaySlipEndpoint GetAllPaySlipsForSuperRateEndpoint = new("/pay-slips/super-rate", "get-all-pay-slips-for-super-rate", "GET");
-        public static PaySlipEndpoint GetAllPaySlipsForAnnualSalaryEndpoint = new("/pay-slips/annual-salary", "get-all-pay-slips-for-annual-salary", "GET");
+        public static PaySlipEndpoint GetAllPaySlipsForEmployeeEndpoint = new("archive/pay-slips/employee", "get-all-pay-slips-for-employee", "GET");
+        public static PaySlipEndpoint GetAllPaySlipsForSuperRateEndpoint = new("archive/pay-slips/super-rate", "get-all-pay-slips-for-super-rate", "GET");
+        public static PaySlipEndpoint GetAllPaySlipsForAnnualSalaryEndpoint = new("archive/pay-slips/annual-salary", "get-all-pay-slips-for-annual-salary", "GET");
     }
 }
